9f7d9c34ab9234a3679a5b9bd69897bbf0b21a47
mafb <malgorzata.faber@ailleron.com>
2018-11-14 16:48:01 +0100

    add user

--- src/test/java/Framework/user.java
+++ src/test/java/Framework/user.java
@@ -8,6 +8,10 @@ public final class User {
     public String language;
     public String location;
     public Personal personal;
+
+
+
+
     public final class Personal{
         public String hobby;
         public int age;
